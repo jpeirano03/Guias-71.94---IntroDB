9b2a08d8bfd544f6ba29936fd1bfd35d86b6ea1e
Juan Manuel Peirano <168123797+jpeirano03@users.noreply.github.com>
2025-08-19 18:50:39 -0300

    Update ej5.sql

--- guia1/ej5.sql
+++ guia1/ej5.sql
@@ -1,3 +1,9 @@
 SELECT e.first_name, e.last_name, e.salary
 FROM EMPLOYEE e
 WHERE e.manager_id IS NULL;
+
+
+-- Concatenando el nombre
+SELECT e.last_name||', '||e.first_name empleado, e.salary
+FROM EMPLOYEE e
+WHERE e.manager_id IS NULL;
